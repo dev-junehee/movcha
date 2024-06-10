71de34e05c477fcd4e215194a31a1fb7fdd2ced2
dev-junehee <dev.junehee@gmail.com>
2024-06-10 16:17:43 +0900

    Refactor: Model 파일 분리 (Box Office, Trending)

---
+++ movcha/Model/Trending.swift
@@ -0,0 +1,32 @@
+//
+//  Trending.swift
+//  movcha
+//
+//  Created by junehee on 6/10/24.
+//
+
+
+struct Trending: Decodable {
+    let page: Int
+    let total_pages: Int
+    let results: [TrendingResults]
+    let total_results: Int
+}
+
+struct TrendingResults: Decodable {
+    let id: Int
+    let title: String
+    let original_title: String
+    let overview: String
+    let poster_path: String
+    let backdrop_path: String
+    let media_type: String
+    let adult: Bool
+    let original_language: String
+    let genre_ids: [Int]
+    let popularity: Double
+    let release_date: String
+    let video: Bool
+    let vote_average: Double
+    let vote_count: Int
+}
