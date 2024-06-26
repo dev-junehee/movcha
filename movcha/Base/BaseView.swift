c5a2e30797865571db6f99cc1ae3a0cdbd8ba20b
dev-junehee <dev.junehee@gmail.com>
2024-06-26 22:14:31 +0900

    Refactor: BaseView 구현, 홈 탭 메인 화면 BaseView 적용, Search-Recommend  폴더 이동

---
+++ movcha/Base/BaseView.swift
@@ -0,0 +1,8 @@
+//
+//  BaseView.swift
+//  movcha
+//
+//  Created by junehee on 6/26/24.
+//
+
+import Foundation
---
+++ movcha/Controllers/Home/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  movcha
+//
+//  Created by junehee on 6/26/24.
+//
+
+import Foundation
